8e8d068c3b8595202e30ef1febabb5b0f4ecf3f5
ledoan05 <lecongdoan05@gmail.com>
2025-10-07 20:50:48 +0700

    reDeploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Lấy __dirname trong môi trường ES Module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,9 +13,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  },
-  base: "./",
-  build: {
-    outDir: "dist",
   },
 });
